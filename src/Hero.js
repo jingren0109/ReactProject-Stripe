bc13ccc5e2e207d64ba0997bb2866315d5e7732e
Jing Ren <jingren@192.168.1.112>
2023-07-23 13:31:53 +0100

    first commit

---
+++ src/Hero.js
@@ -0,0 +1,30 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payment infrastructure or the internet</h1>
+          <p>
+            Payment infrastructure or the internet. Payment infrastructure or
+            the internet. Payment infrastructure or the internet. Payment
+            infrastructure or the internet. Payment infrastructure or the
+            internet. Payment infrastructure or the internet. Payment
+            infrastructure or the internet. Payment infrastructure or the
+            internet.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
